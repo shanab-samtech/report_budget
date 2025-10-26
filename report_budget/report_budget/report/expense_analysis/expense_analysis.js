e4d8a87c17b355fc7aa4ffcf2ff9510cdf133d78
Mostafa Shanab <m_shanab@samtech.me>
2025-10-26 14:38:05 +0300

    removed fiscal year filter and make dimension filter default from its company's default cost center

--- report_budget/report_budget/report/expense_analysis/expense_analysis.js
+++ report_budget/report_budget/report/expense_analysis/expense_analysis.js
@@ -3,6 +3,45 @@
 
 frappe.query_reports["Expense Analysis"] = {
 	filters: get_filters(),
+	// --- MODIFICATION START: Use onload to set default filter value ---
+	onload: function (report) {
+		// Set the fiscal year defaults based on today's date
+		let today = frappe.datetime.get_today();
+		let current_fiscal_year = erpnext.utils.get_fiscal_year(today);
+		report.set_filter_value("from_fiscal_year", current_fiscal_year);
+		report.set_filter_value("to_fiscal_year", current_fiscal_year);
+
+		// 1. Get the current default company
+		let company = report.get_filter_value("company");
+
+		// 2. Only proceed if the default dimension is Cost Center
+		if (report.get_filter_value("budget_against") === "Cost Center") {
+			// 3. Fetch the default cost center asynchronously or use a synchronous call
+			// Using synchronous call for simplicity in onload, as report filters are ready.
+			frappe.call({
+				method: "frappe.client.get_value",
+				args: {
+					doctype: "Company",
+					fieldname: "cost_center",
+					filters: {
+						name: company,
+					},
+				},
+				async: false, // Ensure this completes before moving on
+				callback: function (r) {
+					if (r.message && r.message.cost_center) {
+						let default_cc = r.message.cost_center;
+
+						// 4. Set the value for the MultiSelectList (must be an array)
+						// This uses set_filter_value, which correctly updates the UI.
+						report.set_filter_value("budget_against_filter", [default_cc]);
+					}
+				},
+			});
+		}
+	},
+	// --- MODIFICATION END ---
+
 	formatter: function (value, row, column, data, default_formatter) {
 		value = default_formatter(value, row, column, data);
 
@@ -62,16 +101,18 @@ function get_filters() {
 			label: __("From Fiscal Year"),
 			fieldtype: "Link",
 			options: "Fiscal Year",
-			default: erpnext.utils.get_fiscal_year(frappe.datetime.get_today()),
 			reqd: 1,
+			hidden: 1,
+			// Removed default here, setting in onload
 		},
 		{
 			fieldname: "to_fiscal_year",
 			label: __("To Fiscal Year"),
 			fieldtype: "Link",
 			options: "Fiscal Year",
-			default: erpnext.utils.get_fiscal_year(frappe.datetime.get_today()),
 			reqd: 1,
+			hidden: 1,
+			// Removed default here, setting in onload
 		},
 		{
 			fieldname: "period",
@@ -111,6 +152,7 @@ function get_filters() {
 			label: __("Dimension Filter"),
 			fieldtype: "MultiSelectList",
 			options: "budget_against",
+			// IMPORTANT: Removed the `default` property here, it is now set in `onload`.
 			get_data: function (txt) {
 				if (!frappe.query_report.filters) return;
 
